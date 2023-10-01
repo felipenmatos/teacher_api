d2e0163bcf4908ff2a72257337362aa7185298bf
Felipe Nonato <notfelipe2@hotmail.com>
2023-10-01 15:47:32 -0300

    feat: implementation of library

---
+++ data/teachers.js
@@ -0,0 +1,34 @@
+const list_teachers = [
+  {
+    id: 1,
+    nome: "Amanda da Conceição",
+    turno: "Vespertino",
+    materia: "Português",
+  },
+  {
+    id: 2,
+    nome: "Talita Bonfim",
+    turno: "Vespertino",
+    materia: "Matemática",
+  },
+  {
+    id: 3,
+    nome: "João Francisco",
+    turno: "Noturno",
+    materia: "Lógica",
+  },
+  {
+    id: 4,
+    nome: "José Barreto",
+    turno: "Noturno",
+    materia: "Lingua Inglesa",
+  },
+  {
+    id: 5,
+    nome: "Vera Lúcia",
+    turno: "Noturno",
+    materia: "Ética",
+  },
+];
+
+module.exports = list_teachers;
