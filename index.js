7912a763a0983224769517502b79d04c50be051c
Felipe Nonato <notfelipe2@hotmail.com>
2023-10-02 10:05:21 -0300

    feat: implementation of the intermediary

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const router = require("./router/router");
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  console.log("The body mensage", req.body);
+  next();
+});
 app.use(router);
 
 app.listen(8000);
