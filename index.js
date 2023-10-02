f7addcebf09a270d910635d5e798c46e40378f47
Felipe Nonato <notfelipe2@hotmail.com>
2023-10-02 07:04:17 -0300

    style: server reorganization

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const express = require("express");
-const controllers = require("./controllers/controllers");
+const router = require("./router/router");
 
 const app = express();
 app.use(express.json());
-
-app.get("/professores", controllers.consultTeachers);
-app.get(
-  "/professores/:idConsultado",
-  controllers.consult_a_Teacher
-);
-app.post("/professores", controllers.register_a_Teacher);
-app.patch(
-  "/professores/:idConsultado",
-  controllers.edit_Teacher
-);
-app.put(
-  "/professores/:idConsultato",
-  controllers.replace_or_create
-);
-app.delete(
-  "/professores/:idConsultado",
-  controllers.deleteRegistration
-);
+app.use(router);
 
 app.listen(8000);
